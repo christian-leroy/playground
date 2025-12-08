04b0963ced796c22b63ee5ab01358ac74d8788b4
Christian Leroy <christian.leroy@gmx.de>
2025-12-08 23:42:26 +0100

    paths

--- js/milonga-cal.js
+++ js/milonga-cal.js
@@ -17,7 +17,7 @@ async function initMilongaCal(){
 
 
 async function getMilongas(){
-  const url = "../data/milongas.json";
+  const url = "./data/milongas.json";
   try {
     const response = await fetch(url);
     if (!response.ok) {
