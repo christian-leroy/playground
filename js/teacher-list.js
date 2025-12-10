b36ab862f16207e8bc6783af83025f2c4ed36206
Christian Leroy <christian.leroy@gmx.de>
2025-12-10 17:48:21 +0100

    enumerator to list

--- js/teacher-list.js
+++ js/teacher-list.js
@@ -25,23 +25,20 @@ async function getTeachers(){
 
 function displayTeachers(){
     let html_buffer = ''
-    let number = 1
     for (const teacher of teachers) {
         const teacherName = teacher.name
         const teacherLoc = teacher.location
         const teacherLink = teacher.link
         html =
-        ` 
-        <article class = "teacher"> 
-        <p class = "enumerator">${number}</p>
+        `
+        <li class = "teacher">
         <div class = teacher-info>
             <h2 class = "teacher-name"><a href=${teacherLink} target="_blank">${teacherName} </a></h2>
             <p class = "teacher-location">${teacherLoc}</p>
         </div>
-        </article>
+        </li>
         `
         html_buffer += html
-        number++
     }
     teacherContainer.insertAdjacentHTML('beforeend', html_buffer)
 }
